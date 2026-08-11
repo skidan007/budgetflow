25ccfb1ec961102edbe69470a2c8d05eb0055e17
Daniel Victor <skidan007@gmail.com>
2026-08-11 18:25:10 +0100

    vercel2.jason added

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()], base: '/'
+  plugins: [react(), tailwindcss()], base: '/budgetflow/'
 });
